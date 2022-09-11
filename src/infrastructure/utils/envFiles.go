cead9d59506bbfa4e059290780535da2a50cc3b9
Caio <caiow.wk@gmail.com>
2022-09-11 17:02:03 -0300

    chore: fix workflow

--- src/infrastructure/utils/envFiles.go
+++ src/infrastructure/utils/envFiles.go
@@ -1,23 +1,23 @@
 package utils
 
-import (
-	"log"
-	"os"
-	"regexp"
+// import (
+// 	"log"
+// 	"os"
+// 	"regexp"
 
-	"github.com/joho/godotenv"
-)
+// 	"github.com/joho/godotenv"
+// )
 
-const projectDirName = "Database-Transactions-Simulation"
+// const projectDirName = "Database-Transactions-Simulation"
 
-func LoadEnv() {
-	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
-	currentWorkDirectory, _ := os.Getwd()
-	rootPath := projectName.Find([]byte(currentWorkDirectory))
+// func LoadEnv() {
+// 	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+// 	currentWorkDirectory, _ := os.Getwd()
+// 	rootPath := projectName.Find([]byte(currentWorkDirectory))
 
-	err := godotenv.Load(string(rootPath) + "/.env")
+// 	err := godotenv.Load(string(rootPath) + "/.env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file", err)
-	}
-}
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file", err)
+// 	}
+// }
