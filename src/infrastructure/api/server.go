4bd20348c3d02efc652061968007ad6296a12619
Caio <caiow.wk@gmail.com>
2022-10-26 13:52:13 -0300

    feat: add login endpoint

--- src/infrastructure/api/server.go
+++ src/infrastructure/api/server.go
@@ -29,13 +29,25 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 		store:      store,
 		tokenMaker: tokenMaker,
 	}
-	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+
+	return server, nil
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -44,11 +56,6 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 	router.POST("/transfers/", server.createTransfer)
 
 	server.router = router
-	return server, nil
-}
-
-func (server *Server) Start(address string) error {
-	return server.router.Run(address)
 }
 
 func errorResponse(err error) gin.H {
