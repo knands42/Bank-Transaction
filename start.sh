9b961e427ff3abddd4b3a012e94d169d67d77a5f
Caio <caiow.wk@gmail.com>
2022-11-06 20:37:32 -0300

    chore: load env before migrate

--- start.sh
+++ start.sh
@@ -3,11 +3,8 @@
 set -e
 
 echo "run db migration"
+source app.env
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
-
-echo "installing sqlc"
-go install github.com/kyleconroy/sqlc/cmd/sqlc@latest
-sqlc generate
 
 echo "start the app"
 exec "$@"
